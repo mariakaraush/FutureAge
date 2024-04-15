9f51f417a6ee03fba76c0fa000f346e24f1bd981
mariakaraush <167077657+mariakaraush@users.noreply.github.com>
2024-04-15 13:45:49 +0000

    .

--- age.js
+++ age.js
@@ -1,6 +1,11 @@
-let age = 50;
+let age = 90;
 let numberOfYears = 10;
 let in20Years = numberOfYears * 2;
 console.log(`In 10 years you will be ${age + numberOfYears}.`);
 console.log(`In 20 years you will be ${in20Years }.`);
+
+if (age < 67) {
 console.log(`You have ${ 67 - age } years left to retirement.`);
+} else {
+console.log(`You have retired ${age - 67} years ago. Enjoy your retirment.`);
+}
