8cadb36d45682983d3ad689bbe7adf763fbf96d0
mariakaraush <167077657+mariakaraush@users.noreply.github.com>
2024-04-15 20:15:55 +0100

    Update dogYears.js

--- dogYears.js
+++ dogYears.js
@@ -1,7 +1,10 @@
-let dogAge = 15
+const readline = require('readline-sync')
+let dogAge = Math.floor(readline.question("Please enter the dog's age: "))
+
 let earlyDogYears = 2
 let lateDogYears = dogAge - earlyDogYears
 
 earlyDogYears = earlyDogYears * 10.5
 
+console.log(`The dog's age in human years is ${dogAge} years old. The dog's age in dog's years is ${lateDogYears * 4 + earlyDogYears}`)
 
