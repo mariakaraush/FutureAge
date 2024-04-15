873300f1d8eb4e04c9b9145bb9fd90f2a09fb449
mariakaraush <167077657+mariakaraush@users.noreply.github.com>
2024-04-15 13:40:22 +0000

    Calculate years left for retirment

--- age.js
+++ age.js
@@ -2,4 +2,5 @@ let age = 50;
 let numberOfYears = 10;
 let in20Years = numberOfYears * 2;
 console.log(`In 10 years you will be ${age + numberOfYears}.`);
-console.log(`In 10 years you will be ${age + numberOfYears}.`);
+console.log(`In 20 years you will be ${in20Years }.`);
+console.log(`You have ${ 67 - age } years left to retirement.`);
