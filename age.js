67fdaf2f3c8d4576c1fc1aacadad0a97ee1c0c81
mariakaraush <167077657+mariakaraush@users.noreply.github.com>
2024-04-15 13:28:58 +0000

    .

--- age.js
+++ age.js
@@ -1,1 +1,5 @@
-console.log('Hello There!') 
+let age = 50;
+let numberOfYears = 10;
+let in20Years = numberOfYears * 2;
+console.log(`In 10 years you will be ${age + numberOfYears}.`);
+console.log(`In 10 years you will be ${age + numberOfYears}.`);
