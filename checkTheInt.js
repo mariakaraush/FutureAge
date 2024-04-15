31365de40a3f7d7b15adf7f9d578be10188b2bbc
mariakaraush <167077657+mariakaraush@users.noreply.github.com>
2024-04-15 16:03:00 +0000

    .

--- checkTheInt.js
+++ checkTheInt.js
@@ -1,14 +1,21 @@
 const readline = require('readline-sync')
-let intNum = readline.question('Please enter a number: ')
+let userInput= readline.question('Please enter a number: ')
+let convertUser = parseInt(userInput)
 
-while (intNum != 'number') {
-    intNum = readline.question('Please enter a number: ')
+// isNan()
+
+while (isNaN(convertUser)) {
+    userInput = readline.question('Please enter a number: ')
+    let convertUser = parseInt(userInput)
 }
 
-if (Math.sign(intNum) == 1) {
-    console.log('Your number is positive.')
-} else if (Math.sign(intNum) == -1) {
-    console.log('Your number is negative')
-} else if (Math.sign(intNum) == 0) {
-    console.log('Your number is a zero.')
+if (Math.sign(userInput) == 1) {
+    console.log('Your number is positive.');
+} else if (Math.sign(userInput) == -1) {
+    console.log('Your number is negative');
+} else if (Math.sign(userInput) == 0) {
+    console.log('Your number is a zero.');
 }
+
+
+// typeof(var) == "number"
